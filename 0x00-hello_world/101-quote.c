297ef3f930a4508848de3c2d9a33752570afc9e1
AmiraAshreef <amira.m.ahmed113@gmail.com>
2023-08-10 14:58:03 -0700

    update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,11 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	wprintf(L " and that piece of art is useful" - Dora Korpar, 2015-10-19, \n");
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
