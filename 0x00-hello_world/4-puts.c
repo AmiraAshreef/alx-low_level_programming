fab12d7b878002e8e74ddb7f4fe62315d54c1465
AmiraAshreef <amira.m.ahmed113@gmail.com>
2023-08-10 10:39:44 -0700

    updat 2 for file 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 /**
- * my first program print statment and return 0 
  * main - Entry point
  *
- * Return: 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
