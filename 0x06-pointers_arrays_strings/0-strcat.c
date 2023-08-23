b2328559ea9419cbf498623757ed55425a326e94
AmiraAshreef <amira.m.ahmed113@gmail.com>
2023-08-23 14:25:09 -0700

    Q0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *_strcat - function that concatenates two strings
+ * * @dest: string with concatenation
+ * @src: string to be concatenated
+  * Return: This return to dest, that concatenates two strings
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char   strcat(char s1, char s2);
+	return (dest);
+}
