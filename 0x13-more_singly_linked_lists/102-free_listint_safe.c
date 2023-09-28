5b8946883a1305ba5f881411d5fa66ca19a93c4b
AmiraAshreef <amira.m.ahmed113@gmail.com>
2023-09-27 19:34:36 -0700

    Q 102

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -53,35 +53,32 @@ listint_t *find_listint(listint_t *head)
  */
 size_t free_listint_safe(listint_t **h)
 {
+	listint_t *next, *loopnode;
+	size_t len;
+	int loop = 1;
+
 	if (h == NULL || *h == NULL)
+		return (0);
+
+	loopnode = find_listint_loop_fl(*h);
+	for (len = 0; (*h != loopnode || loop) && *h != NULL; *h = next)
 	{
-	return (0);
+		len++;
+		next = (*h)->next;
+		if (*h == loopnode && loop)
+		{
+			if (loopnode == loopnode->next)
+			{
+				free(*h);
+				break;
+			}
+			len++;
+			next = next->next;
+			free((*h)->next);
+			loop = 0;
+		}
+		free(*h);
 	}
-	listint_t *loop_node;
-
-	*loop_node = *find_listint(*h);
-
-	listint_t *next = (*h)->next;
-	size_t len = 0;
-
-	while (*h != loop_node || loop_node != NULL)
-	{
-	len++;
-	if (*h == loop_node)
-	{
-	break;
-	}
-	free(*h);
-	*h = next;
-	next = next->next;
-	}
-
-	if (loop_node != NULL)
-	{
-	free(loop_node);
-	}
-
 	*h = NULL;
-
 	return (len);
 }
