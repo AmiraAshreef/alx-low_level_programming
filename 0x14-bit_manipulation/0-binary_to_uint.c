8faeff0f655d979e6830f95a58306fed1c2e80c5
AmiraAshreef <amira.m.ahmed113@gmail.com>
2023-09-28 17:44:15 -0700

    Q0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -18,12 +18,11 @@ unsigned int binary_to_uint (const char *b)
 
 	unsigned int len;
 
-	len = strlen(b);
 
 	if (b == NULL)
 	return (0);
 
-	for (i = len - 1; i >= 0; i--)
+	for (i = 0;  b[i] != '\0'; i--)
 	{
 		if (b[i] != '0' && b[i] != '1')
 		return (0);
