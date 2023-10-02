7403fddeb960d2f068841b10a85968784596de4e
AmiraAshreef <amira.m.ahmed113@gmail.com>
2023-10-02 12:09:28 -0700

    Q 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,6 @@
  * Return:1 on success, -1 on failure
  */
 
-
 int create_file(const char *filename, char *text_content)
 {
 
@@ -17,23 +16,21 @@ int create_file(const char *filename, char *text_content)
 	return (-1);
 	}
 
-	int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
-	if (fd < 0)
+	if (text_content != NULL)
 	{
 	return (-1);
 	}
 	if (text_content != NULL)
 	{
-	ssize_t bytes_written = write(fd, text_content, strlen(text_content));
+	int fp  = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (bytes_written < 0)
+	if (fp  < 0)
 	{
-	close(fd);
 	return (-1);
 	}
 	}
-
+	write(file, text_content);
 	close(fd);
 
 	return (1);
