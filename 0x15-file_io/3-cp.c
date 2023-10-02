14bcb70092cd1018cfc966136032c422dfc585a5
AmiraAshreef <amira.m.ahmed113@gmail.com>
2023-10-02 14:44:23 -0700

    Q copy file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,46 +9,45 @@
 
 int main(int argc, char *argv[])
 {
+	int main(int argc, char *argv[])
+{
+int f-org, f-copy;
+int z1 = 1024, n = 0;
+char buffer[1024];
 
-	if (argc != 3)
-	{
-	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-	exit(USAGE_ERROR);
-	}
-	int fd_src = open(argv[1], O_RDONLY);
-	if (fd_src < 0)
-	{
+if (argc != 3)
+	dprintf(STDERR_FILENO, "Usage: cp f-org f-copy\n"), exit(97);
+f-org = open(argv[1], O_RDONLY);
+if (f-org == -1)
+{
 	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-	exit(FILE_READ_ERROR);
-	}
-   int fd_dst = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 
-			S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-	if (fd_dst < 0)
+	exit(98);
+}
+f-copy = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR
+	| S_IRGRP | S_IWGRP | S_IROTH);
+if (f-copy == -1)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+	close(f-org), exit(99);
+}
+while (z == 1024)
+{
+	z = read(f-org, buffer, 1024);
+	if (z == -1)
 	{
-	dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
-	exit(FILE_WRITE_ERROR);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
-	char buf[BUF_SIZE];
+	n = write(f-copy, buffer, z);
+	if (n < z)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+}
 
-	ssize_t bytes_read;
-	while ((bytes_read = read(fd_src, buf, BUF_SIZE)) > 0)
-	{
-	ssize_t bytes_written = write(fd_dst, buf, bytes_read);
-	if (bytes_written < 0)
-	{
-	dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
-	exit(FILE_WRITE_ERROR);
-	}
-	}
-	if (close(fd_src) < 0)
-	{
-	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_src);
-	exit(FILE_CLOSE_ERROR);
-	}
-	if (close(fd_dst) < 0)
-	{
-	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_dst);
-	exit(FILE_CLOSE_ERROR);
-	}
-	return 0;
+if (close(f-org) == -1)
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f-org), exit(100);
+
+if (close(f-copy) == -1)
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f-copy), exit(100);
+
+return (0);
 }
