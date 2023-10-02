f6a6e7dfebfdf3bd30ba664d9b53aaedddf56e39
AmiraAshreef <amira.m.ahmed113@gmail.com>
2023-10-02 14:58:31 -0700

    Q copy file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,23 +13,23 @@ int orgin_file, copy_file;
 int z  = 1024, n = 0;
 char buffer[1024];
 
-if (argc != 3)
+	if (argc != 3)
 	dprintf(STDERR_FILENO, "Usage: cp orgin_file copy_file\n"), exit(97);
-orgin_file = open(argv[1], O_RDONLY);
-if (orgin_file == -1)
-{
+	orgin_file = open(argv[1], O_RDONLY);
+	if (orgin_file == -1)
+	{
 	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 	exit(98);
-}
-copy_file = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR
+	}
+	copy_file = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR
 	| S_IRGRP | S_IWGRP | S_IROTH);
-if (copy_file == -1)
-{
+	if (copy_file == -1)
+	{
 	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 	close(orgin_file), exit(99);
-}
-while (z == 1024)
-{
+	}
+	while (z == 1024)
+	{
 	z = read(orgin_file, buffer, 1024);
 	if (z == -1)
 	{
@@ -38,14 +38,14 @@ while (z == 1024)
 	}
 	n = write(copy_file, buffer, z);
 	if (n < z)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-}
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	}
 
-if (close(orgin_file) == -1)
+	if (close(orgin_file) == -1)
 	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", orgin_file), exit(100);
 
-if (close(copy_file) == -1)
+	if (close(copy_file) == -1)
 	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", copy_file), exit(100);
 
-return (0);
+	return (0);
 }
