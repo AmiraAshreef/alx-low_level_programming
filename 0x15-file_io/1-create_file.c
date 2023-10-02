a0f228b263c3cfd95198de7bd5f83fe824581dea
AmiraAshreef <amira.m.ahmed113@gmail.com>
2023-10-02 12:13:05 -0700

    Q 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,6 +10,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
+	int i;
 
 	if (filename == NULL)
 	{
@@ -21,6 +22,10 @@ int create_file(const char *filename, char *text_content)
 	{
 	return (-1);
 	}
+	while (text_content[i] != '\0')
+	{
+		i++;
+	}
 	if (text_content != NULL)
 	{
 	int fp  = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
@@ -30,7 +35,7 @@ int create_file(const char *filename, char *text_content)
 	return (-1);
 	}
 	}
-	write(file, text_content);
+	write(file, text_content,i);
 	close(fd);
 
 	return (1);
