02c08937f0485994679373887f894f0be28e7750
AmiraAshreef <amira.m.ahmed113@gmail.com>
2023-10-30 12:14:04 -0700

    task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * create_file - function that creates a file.
@@ -10,29 +10,26 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	size_t text_length,  bytes_written;
+	int i = 0, file;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (text_content == NULL)
+		text_content = "";
 
-	if (fd == -1)
-		return (-1);
 
-	if (text_content != NULL)
+	while (text_content[i] != '\0')
 	{
-		text_length = strlen(text_content);
-		bytes_written = write(fd, text_content, text_length);
-
-		if (bytes_written == -1)
-		{
-			close(fd);
-			return (-1);
-		}
+		i++;
 	}
 
-	close(fd);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (file == -1)
+		return (-1);
+
+	write(file, text_content, i);
+
 	return (1);
 }
